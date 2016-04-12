1651fb72f84ff8ca6788821ded504eb28f18ba43
tehsven <tehsven@gmail.com>
2016-04-11 20:26:37 -0700

    Implement all pawn movement including auto-promote to Queen

--- lib/rchess.rb
+++ lib/rchess.rb
@@ -11,7 +11,7 @@ end
 @w_pieces = %w[♖ ♘ ♗ ♕ ♔ ♙].freeze
 @b_pieces = %w[♜ ♞ ♝ ♛ ♚ ♟].freeze
 
-def to_cell rank, file
+def to_coord rank, file
   return rank.to_i-1, file.ord%97
 end
 
@@ -41,7 +41,7 @@ def print_game
   (0..8).reverse_each do |rank|
     row = rank == 0 ? '  ' : "#{rank} "
     ('a'..'h').each do |file|
-      c_rank, c_file = to_cell(rank, file)
+      c_rank, c_file = to_coord(rank, file)
       row += rank == 0 ? "#{file} " : "#{@board[c_rank, c_file]} "
     end
     puts row
@@ -83,17 +83,41 @@ def get_valid_moves(mode, s_move, *arg)
   valid_moves
 end
 
+def get_valid_pawn_moves(s_rank, s_file, op)
+  valid_moves = []
+  # forward
+  t_rank, t_file = s_rank.send(op, 1), s_file
+  if @board[t_rank, t_file] == ' '
+    valid_moves.push([t_rank, t_file])
+    # forward twice
+    t_rank, t_file = s_rank.send(op, 2), s_file
+    if @board[t_rank, t_file] == ' '
+      valid_moves.push([t_rank, t_file])
+    end
+  end
+  # capture
+  other_pieces = op == :+ ? @b_pieces : @w_pieces
+  [[s_rank.send(op, 1), s_file+1], [s_rank.send(op, 1), s_file-1]].each do |t_cell|
+    t_rank, t_file = t_cell[0], t_cell[1]
+    if other_pieces.include?(@board[t_rank, t_file])
+      valid_moves.push([t_rank, t_file])
+    end
+  end
+  # todo en passent
+  valid_moves
+end
+
 def is_valid move
   unless move[0] =~ /[a-h][1-8]/ && move[1] =~ /[a-h][1-8]/
     return 'syntax'
   end
 
-  s_rank, s_file = to_cell(move[0][1], move[0][0])
-  d_rank, d_file = to_cell(move[1][1], move[1][0])
+  s_rank, s_file = to_coord(move[0][1], move[0][0])
+  d_rank, d_file = to_coord(move[1][1], move[1][0])
   piece = @board[s_rank, s_file]
 
   unless (@turn == 'w' ? @w_pieces : @b_pieces).include?(piece)
-    return "not your piece #{piece}"
+    return "not your piece '#{piece}'"
   end
 
   s_move = [s_rank, s_file]
@@ -110,22 +134,28 @@ def is_valid move
     valid_moves = get_valid_moves(:look, s_move, [-1, -1], [1, -1], [1, 1], [-1, 1], [-1, 0], [1, 0], [0, -1], [0, 1])
   when '♚', '♔'
     valid_moves = get_valid_moves(:fixed, s_move, [-1, -1], [1, -1], [1, 1], [-1, 1], [-1, 0], [1, 0], [0, -1], [0, 1])
-    # todo castle
+    # todo castle, check, move into check
   when '♙'
-    valid_moves = get_valid_moves(:fixed, s_move, [1, 0])
-    # todo two jump, capture, en passent
+    valid_moves = get_valid_pawn_moves(s_rank, s_file, :+)
   when '♟'
-    valid_moves = get_valid_moves(:fixed, s_move, [-1, 0])
+    valid_moves = get_valid_pawn_moves(s_rank, s_file, :-)
   end
   valid_moves.include?(d_move) ? true : "valid moves for #{piece}: #{valid_moves.map{ |move| to_ui(move[0], move[1]) }}"
 end
 
 def make_move move
-  s_rank, s_file = to_cell(move[0][1], move[0][0])
-  d_rank, d_file = to_cell(move[1][1], move[1][0])
+  s_rank, s_file = to_coord(move[0][1], move[0][0])
+  d_rank, d_file = to_coord(move[1][1], move[1][0])
 
   @board[d_rank, d_file] = @board[s_rank, s_file]
   @board[s_rank, s_file] = ' '
+  # promotion, auto-promote to queen
+  if '♙' == @board[d_rank, d_file] && d_rank == 7
+    @board[d_rank, d_file] = '♕'
+  end
+  if '♟' == @board[d_rank, d_file] && d_rank == 0
+    @board[d_rank, d_file] = '♛'
+  end
   @turn = @turn == 'w' ? 'b' : 'w'
 end
 
